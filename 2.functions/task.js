177e35ae1c8986f512e1a45394addaef1306b089
Beto4ka <ibritvingithub@mail.ru>
2022-09-22 13:35:25 +0300

    new6 commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -20,14 +20,12 @@ function getArrayParams(myarr = [1, 5, 6, 2, 3]) {
   avg = + (sum / myarr.length).toFixed(2);
   return { min, max, avg };
 }
-console.log(getArrayParams());
+getArrayParams();
 
 
 
 // Задание 2
 
-'use strict';
-let arr = [];
 function worker(arr) {
   let sum = 0;
   for (i = 0; i < arr.length; i++) {
@@ -35,43 +33,49 @@ function worker(arr) {
   }
   return sum;
 }
-//let arrOfArr = [[1, 2, 3, 4], [10, 20, -10, -20]];
 function makeWork(arrOfArr, func) {
   let max = 0;
   for (let i = 0; i < arrOfArr.length; i++) {
-    func;
+    worker(arrOfArr[i]);
+    let func = worker(arrOfArr[i]);
     if (max <= func) {
       max = func;
     }
   }
   return max;
 }
-console.log(makeWork([[1, 2, 3, 4], [10, 20, -10, -20]], worker(arr)));
+
+/*console.log(worker([1, 2, 3])); // 6
+console.log(worker([4, 5, 6])); // 15
+let arrOfArr = [[1, 2, 3], [4, 5, 6]];
+*/
+makeWork(arrOfArr, worker);
 
 
 // Задание 3
 
-'use strict';
-let arr1 = [];
-function worker2(arr1) {
+function worker2(arr) {
   let max = 0;
   let min = 0;
   let difference = 0;
-  difference = Math.max(...arr1) - Math.min(...arr1);
+  difference = Math.max(...arr) - Math.min(...arr);
 
   return Math.abs(difference);
 }
-console.log(worker2([-10, -20, -40])); // -40 - (-10) = -30 => 30
-console.log(worker2([10, 20, 30])); // 30 - 10 = 20
-
 function makeWork(arrOfArr, func) {
   let max = 0;
   for (let i = 0; i < arrOfArr.length; i++) {
-    if (max < func) {
+    worker2(arrOfArr[i]);
+    let func = worker2(arrOfArr[i]);
+    if (max <= func) {
       max = func;
     }
   }
   return max;
 }
-
-console.log(makeWork(arrOfArr = [[-10, -20, -40], [10, 20, 30]], worker2));
+/*
+console.log(worker2([-10, -20, -40])); // -40 - (-10) = -30 => 30
+console.log(worker2([10, 20, 30])); // 30 - 10 = 20
+arrOfArr = [[-10, -20, -40], [10, 20, 30]];
+*/
+makeWork(arrOfArr, worker2); // 30
