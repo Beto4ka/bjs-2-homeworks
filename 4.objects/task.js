407a08f845f0aba20c5c0dd759d5cd66a3f1c7d4
Beto4ka <ibritvingithub@mail.ru>
2022-10-05 11:42:45 +0300

    new10 commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,38 @@
+
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age =
 
+    Student.prototype.setSubject = function (subjectName) {
+      this.subject = subjectName;
+    }
+
+  Student.prototype.addMark = function (mark) {
+    if (this.marks === undefined) {
+      this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+  }
+
+  Student.prototype.addMarks = function (...marks) {
+    if (this.marks === undefined) {
+      this.marks = [...marks];
+    } else {
+      this.marks.push(...marks);
+    }
+  }
+
+  Student.prototype.getAverage = function (marks) {
+    let sum = this.marks.reduce((acc, marks) => acc + marks, 0);
+    let length = this.marks.length;
+    return sum / length;
+  }
+
+  Student.prototype.exclude = function (reason) {
+    this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
+  }
 }
-
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
-
-// ваш код для остальных методов
