932a578855ba1b2beff05ce51277a63057531d04
Beto4ka <ibritvingithub@mail.ru>
2022-10-05 11:00:38 +0300

    new8 commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
+
 function compareArrays(arr1, arr2) {
-  let result;
 
-  // Ваш код
+  let result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index])
 
-  return result; // boolean
+  return result
 }
 
 function advancedFilter(arr) {
-  let resultArr;
 
-  // Ваш код
+  let resultArr = arr.filter((element) => element > 0).filter((element) => element % 3 === 0).map((element) => element * 10)
 
-  return resultArr; // array
+  return resultArr;
 }
